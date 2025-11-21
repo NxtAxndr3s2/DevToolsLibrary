804865b00c2ec7910a20cf4d14847205b9a89cbe
unknown <justandres375@gmail.com>
2025-11-21 16:29:17 -0500

    Commit 3: Se añadio codigo js en el archivo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-fjiapñajgipkawgpñakgmokwgajim 
+<script>
+function searchMenu() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let items = document.querySelectorAll(".menu_list li");
+
+    items.forEach(item => {
+        let text = item.innerText.toLowerCase();
+        if (text.includes(input)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
+</script>
